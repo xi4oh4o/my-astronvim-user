82e66228e8582ff9eb28ec0661d7f7c37d791a6d
xi4oh4o <xi4oh4o@gmail.com>
2023-03-13 12:57:24 +0800

    feat(mappings.lua): added custom mapping

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,8 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    [",s"] = { "<cmd>w<cr>", desc = "Save" },
+    [",q"] = { "<cmd>q<cr>", desc = "Quit" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
