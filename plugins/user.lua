3c30d2495cc402d63d1297c3ef23ad3e641b27c8
xi4oh4o <xi4oh4o@gmail.com>
2023-03-23 16:42:41 +0800

    feat(plugins): remove unused

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,7 +14,12 @@ return {
     event = "BufRead",
     config = function()
       require("transparent").setup({
-        enable = true, -- boolean: enable transparent
+        groups = { -- table: default groups
+          'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+          'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+          'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+          'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+        },
         extra_groups = { -- table/string: additional groups that should be cleared
           -- In particular, when you set it to 'all', that means all available groups
 
@@ -26,8 +31,7 @@ return {
           "BufferLineSeparator",
           "BufferLineIndicatorSelected",
         },
-        exclude = {}, -- table: groups you don't want to clear
-        ignore_linked_group = true, -- boolean: don't clear a group that links to another group
+        exclude_groups = {}, -- table: groups you don't want to clear
       })
     end
   },
