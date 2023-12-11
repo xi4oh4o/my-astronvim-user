345b1ee0f10a04f40694c2379dba0eb84c857f67
xi4oh4o <xi4oh4o@gmail.com>
2023-12-11 13:13:41 +0800

    feat(init): update plugins

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "astromars",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
