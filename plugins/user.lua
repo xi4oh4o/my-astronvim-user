3c1ffb93e90b15990c93f2f7ca5ece99ce57a529
xi4oh4o <xi4oh4o@gmail.com>
2023-12-14 14:23:57 +0800

    feat(plugins): added flash

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,10 +9,6 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   event = "BufRead"
-  -- },
   {
     "xiyaowong/nvim-transparent",
     event = "BufRead",
@@ -39,27 +35,4 @@ return {
       })
     end
   },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
-  }
-  -- {
-  --   "kylechui/nvim-surround",
-  --   event = "BufRead",
-  --   config = function()
-  --       require("nvim-surround").setup({
-  --           -- Configuration here, or leave empty to use defaults
-  --       })
-  --   end
-  -- }
 }
